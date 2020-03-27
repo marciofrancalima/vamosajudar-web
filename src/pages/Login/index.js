8328f87ef9ffc8ab325422d67c5496f63f0bcb14
Márcio França Lima <marciofrancalima@hotmail.com>
2020-03-27 00:16:57 -0300

    feat:  Add user session

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '~/services/api';
 
 import vamosAjudarImg from '~/assets/vamos-ajudar.png';
 
 import * as S from './styles';
 
 export default function Login() {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      toast.error('Oops... Veja se o ID está correto!');
+    }
+  }
+
   return (
     <S.Container>
       <S.Wrapper>
@@ -14,8 +37,13 @@ export default function Login() {
           <S.Subtitle>Faça seu login</S.Subtitle>
         </S.Header>
 
-        <S.Form>
-          <S.Input placeholder="Sua ID" autoFocus />
+        <S.Form onSubmit={handleLogin}>
+          <S.Input
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            placeholder="Sua ID"
+            autoFocus
+          />
           <S.Button type="submit">Entrar</S.Button>
 
           <S.CustomLink to="/register">
