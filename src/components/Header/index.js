a35b6f942fc0850c7a715b15c9a9149a335e7e07
Márcio França Lima <marciofrancalima@hotmail.com>
2020-03-27 00:29:17 -0300

    chore: Add name of the ong in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { FiPower } from 'react-icons/fi';
 
 import * as S from './styles';
 
 export default function Profile() {
+  const [ongName, setOngName] = useState('');
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const name = localStorage.getItem('ongName');
+
+    if (name) {
+      setOngName(name);
+    } else {
+      history.push('/');
+    }
+  }, [history]);
+
   return (
     <S.Container>
       <S.Header>
         <S.Wrapper>
           <S.Logo>#VamosAjudar</S.Logo>
-          <S.WelcomeMessage>Bem vindo, APAD</S.WelcomeMessage>
+          <S.WelcomeMessage>
+            Bem vindo, &nbsp;
+            {ongName}
+          </S.WelcomeMessage>
         </S.Wrapper>
 
         <S.Wrapper>
